e530726fc82ff62816f23b543045515dfeadf64b
mrmystica <mariarmystica@gmail.com>
2022-02-10 14:20:40 +0100

    l

--- works/mrmystica/langton/src/ant.js
+++ works/mrmystica/langton/src/ant.js
@@ -1,38 +1,63 @@
+import { getPixel, setPixel } from '../../../../common/canvas.js'
 
 let x = 0
 let y = 0
 let orientation = 0
 
+const teleport = (newX, newY) => {
+  x = newX
+  y = newY
+}
+
 const moveForward = () => {
-    if (orientation === 0) {
-      x = x + 1
-    }
-    else if (orientation === 1) {
-      y = y + 1
-    }
-    else if (orientation === 2) {
-      x = x - 1
-    }
-    else if (orientation === 3) {
-      y = y - 1
-    }
+  // Droite :
+  if (orientation === 0) {
+    x = x + 1
   }
-  
-  
-const move = () => {
-    export const turnRight = () => {
-      orientation = orientation + 1
-      if (orientation == 4)
+  // Bas :
+  else if (orientation === 1) {
+    y = y + 1
+  }
+  // Gauche :
+  else if (orientation === 2) {
+    x = x - 1
+  }
+
+  // Haut :
+  else if (orientation === 3) {
+    y = y - 1
+  }
+}
+
+const turnRight = () => {
+  orientation = orientation + 1
+  if (orientation === 4) {
     orientation = 0
-    }
-    
-    export const turnRight = () => {
-      orientation = orientation + 1
-      if (orientation == -1)
+  }
+}
+
+const turnLeft = () => {
+  orientation = orientation - 1
+  if (orientation === -1) {
     orientation = 3
-    }
-    
-    export const move = () => {
-      moveForward()
-      setPixel(x, y, 'red')
-    })
+  }
+}
+
+const move = () => {
+  const pixel = getPixel(x, y)
+
+  if (pixel === '#ff0000') {
+    setPixel(x, y, '#ffffff')
+    turnLeft()
+  } else {
+    setPixel(x, y, '#ff0000')
+    turnRight()
+  }
+
+  moveForward()
+}
+
+export const ant = {
+  move,
+  teleport,
+}
