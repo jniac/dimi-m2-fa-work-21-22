ccd01427282aff834d5bd45d63017fd95cd958dc
apiault <anaelle.piault@gmail.com>
2022-02-17 11:51:12 +0100

    scroll-color

---
+++ works/apiault/SCROLL_MASTER/js/bloc2.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+		$(".color1").click(function(){
+      $('.cont-text').css("color", "#06dbe2");			
+		});
+    $(".color2").click(function(){
+      $('.cont-text').css("color", "#187abb");			
+		});
+    $(".color3").click(function(){
+      $('.cont-text').css("color", "#4ab928");			
+		});
+    $(".color4").click(function(){
+      $('.cont-text').css("color", "#978670");			
+		});
+  });
