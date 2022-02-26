c4b0faadaa732b788cea7d04bf386c725a8c87dd
Foulindra <paul.patry99@gmail.com>
2022-02-26 13:37:48 +0100

    work foulindra

---
+++ works/Foulindra/développement_web_PaulDIMIM2/color-scroll/js/main.js
@@ -0,0 +1,43 @@
+import './color-set.js'
+import './deux.js'
+import './accordion.js'
+import './rond.js'
+
+/* j'appelle toutes les autres pages js ici pour eviter de créer 67 scripts dans l'html */
+
+/* PARALLAX EFFET */
+
+/* 1 variable pour chaque élément scrollable dans la premiere section.
+Avec vitesse de scroll differente pour chacun d'eux afin d'obtenir le fameux effet parallax. 
+J'ai voulu repartir de 0 pour ce code pour pouvoir bien comprendre le js. */
+
+let sky = document.getElementById("sky");
+let stars = document.getElementById("stars");
+let mountain = document.getElementById("mountain");
+let lac = document.getElementById("lac");
+let herbe = document.getElementById("herbe");
+let text = document.getElementById("text");
+let button = document.getElementById("button");
+
+window.addEventListener('scroll', function(){
+
+var value = window.scrollY;
+sky.style.top = value * 0.5 + 'px'; 
+stars.style.top = value * 0.15 + 'px';
+mountain.style.top = value * 0.30 + 'px';
+lac.style.top = value * -0.02 + 'px';
+herbe.style.top = value * 0.10 + 'px';
+text.style.top = value  * 0.50 + 'px'; 
+button.style.top = value  * 0.50 + 'px'; 
+
+})
+
+
+/* VOLUME MUSIC */ 
+
+  var audio = document.getElementById("music");
+  audio.volume = 0.1;
+
+
+/* commande js pour regler le volume de la musique, j'ai voulu voir si il était possible de regler le
+son d'une musique via css mais j'ai pas trouvé.. mais le js fait l'affaire. */ 
