f5719578d5c6e05c33abf279e58580c8c9025eac
toto <theo.rmgc@gmail.com>
2022-03-01 21:36:05 +0100

    Rendu-Theo/Steph

--- works/TheoRmgc/langton/src/main.js
+++ works/TheoRmgc/langton/src/main.js
@@ -1,8 +1,15 @@
 import { resizeCanvas, setPixel } from '../../../../common/canvas.js'
 import './ant.js'
+import { left, move } from './ant.js'
 
 resizeCanvas(32, 32)
 
 setPixel(3, 3, 'red')
 setPixel(4, 3, 'blue')
 setPixel(4, 4, '#f59833')
+
+move()
+move()
+move()
+left()
+move()
